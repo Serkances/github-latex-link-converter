8991ee18b49ca6aafae8bb5eccd3732245406dc0
Serkances <serkancesme98@gmail.com>
2022-02-03 17:14:54 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toGithubRenderURL() {
+    var input = document.getElementsByName('input')[0].value;
+    var output = '<img src="https://render.githubusercontent.com/render/math?math=' + encodeURIComponent(input) + '">';
+    document.getElementsByName('output')[0].value = output;
+    document.getElementById('result').innerHTML = output;
+}
